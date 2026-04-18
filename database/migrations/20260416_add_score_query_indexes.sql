180288cb005bc481c01203d20e47618681e4dd3f
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2026-04-18 14:55:01 +0800

    chore: remove migrations sql
    Signed-off-by: SiYao Mo <1388852+msylgj@users.noreply.github.com>

--- database/migrations/20260416_add_score_query_indexes.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-CREATE INDEX IF NOT EXISTS idx_scores_event_created_id
-ON scores (event_id, created_at DESC, id DESC);
-
-CREATE INDEX IF NOT EXISTS idx_event_registrations_year_points_lookup
-ON event_registrations (year, points_bow_type, season, name, distance, competition_bow_type);
-
-COMMIT;
--- database/migrations/20260418_add_scores_gender_group.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE scores
-ADD COLUMN IF NOT EXISTS gender_group VARCHAR(50) DEFAULT NULL;
-
-ALTER TABLE scores
-ADD CONSTRAINT ck_scores_gender_group CHECK (gender_group IS NULL OR gender_group IN ('men', 'women', 'mixed'));
-
-COMMIT;
